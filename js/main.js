937fa1732dcae5b62a3ef944bc56cad41428ef29
jayyange <86558126+jayyange@users.noreply.github.com>
2022-03-28 22:44:07 +0300

    kasjdshdfsj
    scnndsn

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Hamisi Yange.", "Lawyer.", "An author."],
+      strings: ["Hamisi Yange.", "A Lawyer.", "An author."],
       typeSpeed: 200,
       loop: true,
     });
